0e462427a6cd0bf6adaf72b468e6c633219c364e
omendra02 <omendra26tomar@gmail.com>
2024-02-29 00:32:05 +0530

    Day20

---
+++ Day20/LongestSubarray1.java
@@ -0,0 +1,44 @@
+package Day20;
+import java.util.*;
+public class LongestSubarray1 {
+    
+    public static int getLongestSubarray(int []a, int k) {
+        int n = a.length; 
+
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+
+            sum += a[i];
+
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+
+            int rem = sum - k;
+
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { -1, 1, 1};
+        int k = 1;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+
